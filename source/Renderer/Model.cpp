74c32970d617624271ae7ffd1b78ed08da4d0a4b
Sherin192 <zorop92@gmail.com>
2020-03-11 18:48:37 +0000

    Allow to set a name for a model and fix name setting when a model is generated now that m_pRootNode stores the model name.

--- source/Renderer/Model.cpp
+++ source/Renderer/Model.cpp
@@ -3,7 +3,7 @@
 #include "Noctis3DTypes.h"
 #include "Engine/ShapeGenerator.h"
 #include "Engine/CameraFPS.h"
-
+#include "Engine/AssetManager.h"
 
 #if defined(_TEMP)
 #define STB_IMAGE_IMPLEMENTATION
@@ -13,10 +13,10 @@
 namespace noctis::rdr
 {
 
-Model::Model(std::shared_ptr<RenderDevice>& renderDevice, sg::Shape shape)
+Model::Model(std::shared_ptr<RenderDevice>& renderDevice, sg::Shape shape, std::string name /* = "" */)
 	: m_cbuffer(renderDevice), m_transform(), m_meshes(), m_pRootNode(new Node)
 {
-	Generate(renderDevice, shape);
+	Generate(renderDevice, shape, name);
 }
 
 
@@ -45,7 +45,7 @@ const Transform& Model::GetTransform() const noexcept { return m_transform; }
 
 
 
-void Model::SetTransform(Transform& t) noexcept { m_transform.Set(t); m_pRootNode->m_transform.Set(t); }
+void Model::SetTransform(const Transform& t) noexcept { m_transform.Set(t); m_pRootNode->m_transform.Set(t); }
 
 
 
@@ -83,35 +83,44 @@ void Model::RenderNode(std::shared_ptr<RenderDevice>& renderDevice, Node* node, 
 
 
 
-void Model::Generate(std::shared_ptr<RenderDevice>& renderDevice, sg::Shape shape)
+void Model::Generate(std::shared_ptr<RenderDevice>& renderDevice, sg::Shape shape, std::string name/* = "" */)
 {
 	sg::ShapeGenerator& sGen = sg::ShapeGenerator::Instance();
 	Data data;
-	std::string name;
+	std::string modelName;
 	switch (shape)
 	{
 	case sg::Shape::SPHERE:
 
 		//Generate the data for the sphere.
-		name = sg::kShapeNameSphere;
+		modelName = sg::kShapeNameSphere;
 		sGen.CreateSphere(2.0f, 10u, 10u, data);
 
 		break;
 	case sg::Shape::CUBE:
 
 		//Generate the data for the cube.
-		name = sg::kShapeNameCube;
+		modelName = sg::kShapeNameCube;
 		sGen.CreateCube(20.0f, 20.0f, 20.0f, data);
 
 		break;
 	
 	case sg::Shape::PLANE:
-		name = sg::kShapeNamePlane;
+		modelName = sg::kShapeNamePlane;
 		sGen.CreatePlane(10.0f, 10.0f, 2, 2, data);
 	default:
 		break;
 	}
-	m_meshes.push_back(std::make_shared<Mesh>(Mesh(renderDevice, name, data.vertices, data.indices)));
+	m_meshes.push_back(std::make_shared<Mesh>(Mesh(renderDevice, modelName, data.vertices, data.indices)));
+	m_pRootNode = std::make_unique<Node>(Node(Transform(), this));
+	m_pRootNode->m_meshes.push_back(0);
+	if (name != "")
+		modelName = name;
+	auto count = AssetManager::Instance().AddName(modelName);
+	if (count)
+		m_pRootNode->m_name = modelName + " (" + std::to_string(count) + ")";
+	else
+		m_pRootNode->m_name = modelName;
 }
 
 
@@ -141,4 +150,9 @@ std::shared_ptr<Material> Model::GetMaterial(const std::string& meshName)
 	}
 	assert(!m_meshes.empty());
 }
+
+
+
+
+void Model::SetName(const std::string& name) noexcept { m_pRootNode->m_name = name; }
 }
--- source/Renderer/Model.h
+++ source/Renderer/Model.h
@@ -28,21 +28,21 @@ class Model : public IRenderable<Model>
 {
 public:
 	Model() = default;
-	Model(std::shared_ptr<RenderDevice>& renderDevice, sg::Shape shape);
+	Model(std::shared_ptr<RenderDevice>& renderDevice, sg::Shape shape, std::string name = "");
 	Model(std::shared_ptr<RenderDevice>& renderDevice, std::string_view path);
 
 	void				Render(std::shared_ptr<RenderDevice>&, Camera&);
 	void				RenderNode(std::shared_ptr<RenderDevice>&, Node*, Camera&, Transform&);
-	void				Generate(std::shared_ptr<RenderDevice>& renderDevice, sg::Shape shape);
+	void				Generate(std::shared_ptr<RenderDevice>& renderDevice, sg::Shape shape, std::string name = "");
 
 	Transform&			GetTransform() noexcept;
 	const Transform&	GetTransform() const noexcept;
-	void				SetTransform(Transform& t) noexcept;
+	void				SetTransform(const Transform& t) noexcept;
+	void				SetName(const std::string& name) noexcept;
 	void				SetMaterial(const std::string& name, const std::string& mesh);
 	std::shared_ptr<Material>	GetMaterial(const std::string& mesh);
 	const				std::vector<std::shared_ptr<Mesh>>& GetMeshes() { return m_meshes; }
 	Node*				GetRootNode() { return m_pRootNode.get(); }
-
 private:
 	//TODO: Model class will need to be redesigned to better represent a node based structure
 	//		which will better fit a futre ECS implementation.
@@ -70,6 +70,5 @@ public:
 
 	friend Model;
 };
-
 }
 #endif //MODEL_H_
